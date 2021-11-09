4b1dc545452acebce3086eb5a816dbfc28bd3293
Patmayanti <60900119027@uin-alauddin.ac.id>
2021-11-09 11:47:46 +0800

    commit_Bontang, 11 April 2000

--- resources/views/mid.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <title>Mid Patmayanti Kartini</title>
-    <body>
-        <p>1. Version Control System adalah salah satu bentuk sistem kontrol yang membantu 
-            developer untuk berkolaborasi dalam mengerjakan sebuah project</p>
-        <p>2.Adapun tools yang dapat digunakan seperti github, sourcetree, GIT, </p>
-</body>
-    </html>
