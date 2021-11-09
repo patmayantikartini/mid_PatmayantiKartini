4de6ef5998812a3f609a3420c602d74cd4d9ed07
Patmayanti <60900119027@uin-alauddin.ac.id>
2021-11-09 12:04:44 +0800

    commit_Bontang, 11 April 2000

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -4,6 +4,6 @@
     <body>
         <p>Version Control System adalah sistem kontrol yang dapat memudahkan
             developer untuk mengembangkan software secara kolaboratif</p>
-        <p>Contoh penerapan version control sistem sendiri seperti pada github, sourcetree, git, bazaar dan subversion</p>
+        <p>Contoh penerapan version control sistem sendiri seperti pada github,sourcetree, git, bazaar dan subversion</p>
 </body>
     </html>
