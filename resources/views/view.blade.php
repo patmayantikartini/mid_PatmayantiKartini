1629e47d030db8c425aeed35536b6411a599d3d8
Patmayanti <60900119027@uin-alauddin.ac.id>
2021-11-09 12:02:50 +0800

    commit_akhir

---
+++ resources/views/view.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <title>Mid Patmayanti Kartini</title>
+    <body>
+        <p>Version Control System adalah sistem kontrol yang dapat memudahkan
+            developer untuk mengembangkan software secara kolaboratif</p>
+        <p>Contoh penerapan version control sistem sendiri seperti pada github, sourcetree, git, bazaar dan subversion</p>
+</body>
+    </html>
